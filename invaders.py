46032c3541341e5eaac6c15373661f8b33bf4a86
Ido Zada <idozada1993@gmail.com>
2020-11-06 16:02:27 +0200

    Initial commit

---
+++ invaders.py
@@ -0,0 +1,27 @@
+import pygame, os, sys
+from pygame.locals import *
+
+# Our imports
+from raspigame import *
+from interstitial import *
+from menu import MainMenuState
+from invadersgame import PlayGameState
+
+"""
+----------------------------------------------------------------------------------------------------
+	Application Entry Point
+				
+	Main entry point to the application. Sets up the objects and starts the main loop.
+----------------------------------------------------------------------------------------------------
+"""
+SCREEN_W = 800
+SCREEN_H = 600
+
+invadersGame = RaspberryPiGame("Invaders", SCREEN_W, SCREEN_H)
+mainMenuState = MainMenuState( invadersGame )
+gameOverState = InterstitialState( invadersGame, 'G A M E  O V E R !', 5000, mainMenuState )
+playGameState = PlayGameState( invadersGame, gameOverState )
+getReadyState = InterstitialState( invadersGame, 'Get Ready!', 2000, playGameState )
+mainMenuState.setPlayState( getReadyState )
+
+invadersGame.run( mainMenuState )
---
+++ menu.py
@@ -0,0 +1,69 @@
+import pygame, os, sys
+from pygame.locals import *
+
+from raspigame import *
+from pygamefont import *
+
+
+class MainMenuState(GameState):
+	
+	def __init__(self, game):
+		super(MainMenuState, self).__init__(game)
+		self.playGameState = None
+		self.optionGameState = None
+		self.font = FontType("david", 50, (255, 255, 255))
+		self.index = 0
+		self.inputTick = 0
+		self.menuItems = ['Start Game', 'Options', 'Quit']
+		
+	def setPlayState(self, state):
+		self.playGameState = state
+
+	def setOptionState(self,state):
+		self.optionGameState = state
+
+	def update(self, gameTime):
+		
+		keys = pygame.key.get_pressed()
+		if ( (keys[K_UP] or keys[K_DOWN]) and self.inputTick == 0):
+			self.inputTick = 250
+			if ( keys[K_UP] ):
+				self.index -= 1
+				if (self.index < 0):
+					self.index = len(self.menuItems) -1
+			elif ( keys[K_DOWN] ):
+				self.index += 1
+				if (self.index == len(self.menuItems)):
+					self.index = 0
+		elif ( self.inputTick >0 ):
+			self.inputTick -= gameTime
+		
+		if ( self.inputTick < 0 ):
+			self.inputTick = 0
+			
+		if keys[K_RETURN]:
+			if self.index == 2:
+				self.game.changeState(None) # exit the game
+			# TODO: RETURN to MainMenu
+			elif self.index == 1:
+				self.game.changeState(self.optionGameState)
+			elif self.index == 0:
+				print (self.playGameState)
+				self.game.changeState(self.playGameState)
+				
+	def draw(self, surface):
+		
+		self.font.centre(surface, "Invaders! From Space!", 48)
+		
+		count = 0
+		y = surface.get_rect().height - len(self.menuItems)*110
+		for item in self.menuItems:
+			itemText = "  "
+			
+			if ( count == self.index ):
+				itemText = "> "
+				
+			itemText += item
+			self.font.draw(surface, itemText, 300, y)
+			y += 50
+			count += 1
